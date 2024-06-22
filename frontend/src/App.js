2a29a0c2f0f1c246171eb0e8ee11691a97c9aa10
RuthwikReddy75 <ruthwikreddy096@gmail.com>
2024-06-22 16:28:14 +0530

    added all files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Firstdis from "./components/firstdis";
+import "./styles/App.css";
+function App()
+{
+  return(
+    <div>
+     
+      <Navbar></Navbar>
+      <Firstdis></Firstdis>
+
+    </div>
+  )
+}
+export default App;
