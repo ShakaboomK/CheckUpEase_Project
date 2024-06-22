5e9b24cf89cb003240d56780ca979cba7c681dce
RuthwikReddy75 <ruthwikreddy096@gmail.com>
2024-06-22 16:10:38 +0530

    src/components added

---
+++ frontend/src/components/firstdis.js
@@ -0,0 +1,25 @@
+import React from "react";
+function Firstdis()
+{
+  return(
+    <div id="firstdis">
+        <div id="content">
+            <h1>Now Diagnostic Center <br></br> At Your Door </h1>
+            <br></br>
+            <br></br>
+            <h1>No More Waiting</h1>
+        </div> 
+      
+        <div id="buttons">
+            <button>Book Appointment</button>
+            <button>Need Suggestion!!</button>
+
+        </div>
+     
+
+
+
+    </div>
+  )
+}
+export default Firstdis;
