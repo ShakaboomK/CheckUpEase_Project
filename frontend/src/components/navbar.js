49a6554eeab54bdeebe93cce18b62c535ab0bb1e
RuthwikReddy75 <ruthwikreddy096@gmail.com>
2024-06-30 16:18:33 +0530

    landing

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -4,25 +4,25 @@ function Navbar()
     return(
         <div id="navbar">
             <div>
-                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpCOXuliXld2Nd_saKc23nvEaEbTFUsmCFyA&s"></img>
+                <img className="logo"></img>
             </div>
             <div id="search">
                 <input type="text" placeholder="search" id="inf"></input>
 
             </div>
-            <div>
+            <div class="navitems">
                 <h3><strong>Find Nearest Center</strong></h3>
             </div>
-            <div>
+            <div class="navitems">
                 <h3><strong>Plan Your Visit</strong></h3>
             </div>
-            <div>
+            <div class="navitems">
                 <h4><strong>Your Diagnosis Result</strong></h4>
             </div>
-            <div>
+            <div class="navitems">
                 <h3><strong>Regular Checkup</strong></h3>
             </div>
-            <div>
+            <div class="navitems">
                 <h3><strong>Login/Signup</strong></h3>
             </div>
 
