815bd79c28a5ff6eec2eac61569fc9be8dcba4e4
Kalva Karthik <mrkarthikkalva007@gmail.com>
2024-07-15 14:59:33 +0530

    added diag-user route with the proper router file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const { app } = require('./src/controllers/server.js')
 const cors = require('cors')
 
-// const { db } = require('./src/models/firebase.js')
+const { db } = require('./src/config/firebase.js')
 
 
 const userRoute = require('./src/routes/userRoute.js')
+const diagnosticCenterRoute = require('./src/routes/diagnosticcenter.js')
 
 app.use(cors())
 app.use('/user', userRoute)
+app.use('/diag-user', diagnosticCenterRoute)
 
 
 app.listen(8000, () => {
